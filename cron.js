59b29b4fbcf86dd60a72c00a686b5c05b15f2604
Maxim Obrubov <m.obrubov@yandex.ru>
2021-01-22 13:01:19 +0400

    check cron requests

--- cron.js
+++ cron.js
@@ -4,6 +4,7 @@ const axios = require('axios');
 const URL_LIST = {
   ID: "http://localhost:1337/huntflow/company-id",
   SYNC: "http://localhost:1337/huntflow/sync-candidate",
+  APPLY: "http://localhost:1337/huntflow/apply-candidate",
 }
 
 module.exports = {
@@ -17,7 +18,22 @@ module.exports = {
         console.log("========================================");
       }).catch((err) => {
         console.log("ERROR IS");
-        console.log(err);
+        console.log(err.response);
+        console.log("========================================");
+      });
+
+    });
+    
+    cron.schedule('*/10 * * * * *', () => {
+      console.log(`Apply cron job at ${Date()}`);
+      
+      axios.get(URL_LIST.APPLY).then((res) => {
+        console.log("RESPONSE IS");
+        console.log(res.data);
+        console.log("========================================");
+      }).catch((err) => {
+        console.log("ERROR IS");
+        console.log(err.response);
         console.log("========================================");
       });
 
